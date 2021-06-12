3de7ec2de6d87d34f60b12afb8e4438629b63537
Thalles <thallescaltabiano@gmail.com>
2021-06-12 19:55:37 -0300

    Produto e Associal N X N com Categoria

--- src/main/java/com/thallescaltabiano/cursomc/domain/Categoria.java
+++ src/main/java/com/thallescaltabiano/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.thallescaltabiano.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Categoria implements Serializable{
@@ -15,6 +18,9 @@ public class Categoria implements Serializable{
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<>();
 	
 	public Categoria() {
 		
@@ -41,6 +47,14 @@ public class Categoria implements Serializable{
 	public String getNome() {
 		return nome;
 	}
+	
+	public List<Produto> getProdutos() {
+		return produtos;
+	}
+
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
 
 	@Override
 	public int hashCode() {
@@ -66,6 +80,4 @@ public class Categoria implements Serializable{
 			return false;
 		return true;
 	}
-	
-	
 }
